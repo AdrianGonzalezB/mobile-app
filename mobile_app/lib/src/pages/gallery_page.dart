b933176e34f72cbbc08cc9542c3eabb2d9683bbb
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-11 20:39:19 +0100

    update Gallery_page
    - quitado image_picker
    - añadido http
    - update gallery state

--- mobile_app/lib/src/pages/gallery_page.dart
+++ mobile_app/lib/src/pages/gallery_page.dart
@@ -1,4 +1,6 @@
-import 'dart:io';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 
@@ -21,12 +23,12 @@ class Gallery extends StatefulWidget {
 
 class _GalleryState extends State<Gallery> {
   bool loading = false;
-  List<File> images = [];
+  List<String> images = [];
 
   @override
   void initState() {
     loading = true;
-    images = [];
+    images = ['0', '10', '1002'];
 
     _loadImages();
     super.initState();
@@ -37,13 +39,20 @@ class _GalleryState extends State<Gallery> {
     return GridView.builder(
       gridDelegate:
           SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-      itemBuilder: (context, index) => Image.network(''),
+      itemBuilder: (context, index) =>
+          Image.network('https://picsum.photos/id/${images[index]}/300/300}'),
       itemCount: images.length,
     );
   }
 
   void _loadImages() async {
-    List<File> _images = [];
+    //var url = Uri.https("https://picsum.photos/v2/list");
+    final response = await http.get(Uri.parse('https://picsum.photos/v2/list'));
+    final json = jsonDecode(response.body);
+    List<String> _images = [];
+    for (var image in json) {
+      _images.add(image['id']);
+    }
     setState(() {
       loading = false;
       images = _images;
