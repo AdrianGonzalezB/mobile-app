f99cc3729103b8393cd96ec20389621768f32383
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-11 19:15:54 +0100

    Update routes.dart

--- mobile_app/lib/src/routes/routes.dart
+++ mobile_app/lib/src/routes/routes.dart
@@ -4,5 +4,6 @@ import 'package:mobile_app/src/pages/pages.dart';
 Map<String, WidgetBuilder> getRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
+    'gallery': (BuildContext context) => GalleryPage(),
   };
 }
