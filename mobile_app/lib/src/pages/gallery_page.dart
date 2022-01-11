dfa8a40020525f5cbe66620a7f667b8dbcab30f9
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-11 19:01:09 +0100

    Update gallery_page.dart

--- mobile_app/lib/src/pages/gallery_page.dart
+++ mobile_app/lib/src/pages/gallery_page.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-class GalleryPage extends StatelessWidget {
-  const GalleryPage({Key? key}) : super(key: key);
+class GalleryPage extends StatefulWidget {
+  GalleryPage({Key? key}) : super(key: key);
+
+  @override
+  _GalleryPageState createState() => _GalleryPageState();
+}
+
+class _GalleryPageState extends State<GalleryPage> {
+  bool loading = false;
+  List<String> images = [];
+
+  @override
+  void initState() {
+    loading = true;
+    images = [];
+
+    _loadImages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (context, index) => Image.network(''),
+      itemCount: images.length,
+    );
+  }
+
+  void _loadImages() async {
+    List<String> _images = [];
+    setState(() {
+      loading = false;
+      images = _images;
+    });
   }
 }
