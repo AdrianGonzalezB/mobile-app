ad315fa46707f12580a0783d3b553afd9c999ca1
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-14 20:11:22 +0100

    update PongGame
    - Ahora se mueve la bola hacia varias direcciones .
    - Ahora el bloque del jugador se mueve de derecha a izquierda con la flecha derecha e izquierda

--- mobile_app/lib/src/pages/pong_page.dart
+++ mobile_app/lib/src/pages/pong_page.dart
@@ -12,43 +12,97 @@ class PongGame extends StatefulWidget {
   _PongGameState createState() => _PongGameState();
 }
 
-enum direction { UP, DOWN }
+enum direction { UP, DOWN, LEFT, RIGHT }
 
 class _PongGameState extends State<PongGame> {
+  // Player variables (Bottom Brick)
+  double playerX = -0.2;
+  double playerWidth = 0.4; // out of 2
+
+  // Ball variables
   double ballX = 0;
   double ballY = 0;
-  var ballDirection = direction.DOWN;
-
+  var ballYDirection = direction.DOWN;
+  var ballXDirection = direction.LEFT;
+  // Game settings
   bool gameStarted = false;
   void startGame() {
     gameStarted = true;
     Timer.periodic(Duration(microseconds: 1), (timer) {
+      // Update direction
       updateDirection();
+      // Ball Movement
       moveBall();
+
+      // Check if the player is dead
+      if (isPlayerDead()) {
+        timer.cancel();
+        // Resets the game
+        resetGame();
+      }
     });
+  }
+
+  void resetGame() {
+    setState(() {
+      gameStarted = false;
+      ballX = 0.0;
+      ballY = 0.0;
+      playerX = -0.2;
+    });
+  }
+
+  bool isPlayerDead() {
+    if (ballX >= 1) {
+      return true;
+    }
+
+    return false;
   }
 
   void updateDirection() {
     setState(() {
-      if (ballY >= 0.9) {
-        ballDirection = direction.UP;
+      // Update vertical direction
+      if (ballY >= 0.9 &&
+          playerX + playerWidth >= ballX &&
+          playerX + playerWidth <= ballX) {
+        ballYDirection = direction.UP;
       } else if (ballY <= -0.9) {
-        ballDirection = direction.DOWN;
+        ballYDirection = direction.DOWN;
       }
+
+      // Update horitzontal direction
+      if (ballX >= 1)
+        ballXDirection = direction.LEFT;
+      else if (ballX <= -1) ballXDirection = direction.RIGHT;
     });
   }
 
   void moveBall() {
     setState(() {
-      if (ballDirection == direction.DOWN)
+      // Vertical movement
+      if (ballYDirection == direction.DOWN)
         ballY += 0.001;
-      else if (ballDirection == direction.UP) ballY -= 0.001;
+      else if (ballYDirection == direction.UP) ballY -= 0.001;
+      // Horitzontal movement
+      if (ballXDirection == direction.LEFT)
+        ballX -= 0.001;
+      else if (ballXDirection == direction.RIGHT) ballX += 0.001;
     });
   }
 
-  void moveLeft() {}
+  void moveLeft() {
+    setState(() {
+      playerX -= 0.05;
+    });
+  }
 
-  void moveRight() {}
+  void moveRight() {
+    setState(() {
+      playerX += 0.05;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return RawKeyboardListener(
@@ -68,9 +122,13 @@ class _PongGameState extends State<PongGame> {
             CoverScreen(
               gameHasStarted: gameStarted,
             ),
-            MyBrick(x: 0.0, y: -0.9),
-            MyBrick(x: 0.0, y: 0.9),
-            MyBall(x: ballX, y: ballY)
+            // Top Brick
+            MyBrick(x: 0.0, y: -0.9, brickWidth: playerWidth),
+
+            // Bottom Brick
+            MyBrick(x: playerX, y: 0.9, brickWidth: playerWidth),
+            // Ball
+            MyBall(x: ballX, y: ballY),
           ])),
         ),
       ),
