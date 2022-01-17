0d0ce677904ebb8f6599ba552d705e6064abcfea
AdrianGonzalezB <adriangonzalez@paucasesnovescifp.cat>
2022-01-17 19:24:50 +0100

    Update utilities.dart

---
+++ mobile_app/lib/src/pages/utilities/utilities.dart
@@ -0,0 +1,4 @@
+export 'package:mobile_app/src/pages/utilities/ball.dart';
+export 'package:mobile_app/src/pages/utilities/brick.dart';
+export 'package:mobile_app/src/pages/utilities/coverscreen.dart';
+export 'package:mobile_app/src/pages/utilities/scorescreen.dart';
